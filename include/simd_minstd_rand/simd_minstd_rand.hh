4ac0e8db779d1b7f1e975a0814754f7d554c0a58
victorbaldin56 <victor.baldin.2005@gmail.com>
2025-05-11 20:01:39 +0000

    fix precision

--- include/simd_minstd_rand/simd_minstd_rand.hh
+++ include/simd_minstd_rand/simd_minstd_rand.hh
@@ -20,14 +20,25 @@ class minstd_rand final {
   auto operator()() noexcept {
     auto vec_mask = _mm512_set1_epi32(0x7fffffff);
     auto vec_multiplier = _mm512_set1_epi32(1098894339);
-    auto product_lo = _mm512_mullo_epi32(state_, vec_multiplier);
-    auto product_hi = _mm512_mul_epi32(state_, vec_multiplier);
+    auto lo = _mm512_mullo_epi32(state_, vec_multiplier);
+    auto a_even = _mm512_cvtepu32_epi64(_mm512_castsi512_si256(state_));
+    auto b_even = _mm512_cvtepu32_epi64(_mm512_castsi512_si256(vec_multiplier));
+    auto a_odd = _mm512_cvtepu32_epi64(_mm512_extracti64x4_epi64(state_, 1));
+    auto b_odd =
+        _mm512_cvtepu32_epi64(_mm512_extracti64x4_epi64(vec_multiplier, 1));
+    auto prod_even = _mm512_mul_epu32(a_even, b_even);
+    auto prod_odd = _mm512_mul_epu32(a_odd, b_odd);
+    auto hi_even = _mm512_srli_epi64(prod_even, 32);
+    auto hi_odd = _mm512_srli_epi64(prod_odd, 32);
+    auto hi = _mm512_mask_blend_epi32(
+        0xaaaa, _mm512_zextsi256_si512(_mm512_cvtepi64_epi32(hi_even)),
+        _mm512_zextsi256_si512(_mm512_cvtepi64_epi32(hi_odd)));
 
-    auto x0 = _mm512_and_epi32(product_lo, vec_mask);
-    auto temp = _mm512_or_epi32(_mm512_srli_epi32(product_lo, 31),
-                                _mm512_slli_epi32(product_hi, 1));
+    auto x0 = _mm512_and_epi32(lo, vec_mask);
+    auto temp =
+        _mm512_or_epi32(_mm512_srli_epi32(lo, 31), _mm512_slli_epi32(hi, 1));
     auto x1 = _mm512_and_epi32(temp, vec_mask);
-    auto x2 = _mm512_srli_epi32(product_hi, 30);
+    auto x2 = _mm512_srli_epi32(hi, 30);
     auto sum = _mm512_add_epi32(x0, x1);
     sum = _mm512_add_epi32(sum, x2);
 
